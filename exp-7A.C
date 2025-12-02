0ab8ffaaf5dd19494da5b6917c31b4de047c9940
lokeshlavate56-abcd <lokeshlavate56@gmail.com>
2025-12-02 20:43:18 +0530

    Add files via upload

---
+++ exp-7A.C
@@ -0,0 +1,26 @@
+//Name:- Lokesh Lavate,UIN:-251P083, Class:-F.E DIV:-D
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, max;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    max = arr[0];
+
+    for(i = 1; i < n; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    printf("Largest element = %d", max);
+
+    return 0;
+}
